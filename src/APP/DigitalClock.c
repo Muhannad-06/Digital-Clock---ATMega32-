44d4f689013f69445340277a80b324056ffd5f70
Muhannad El-Shahiedy <eng.muhannadelshahiedy06@gmail.com>
2026-08-10 00:33:17 +0300

    LCD is DONEgit add .!

---
+++ src/APP/DigitalClock.c
@@ -0,0 +1,23 @@
+#include "../LIBs/STD_TYPES.h"
+#include "../LIBs/BIT_MATH.h"
+
+#include "../MCAL/DIO/DIO_Interface.h"
+
+#include "../MCAL/TIMER/TIMER_Interface.h"
+#include "../MCAL/ADC/ADC_Interface.h"
+
+#include "../HAL/LCD/LCD_Interface.h"
+
+
+int main()
+{
+
+
+
+    while(1)
+    {
+        
+    }
+
+    return 0;
+}
